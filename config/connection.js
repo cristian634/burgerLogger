ba2d1788346d58f6d129bcb8c838d791bd9ff315
cristian634 <cristian634@comcast.net>
2020-05-14 19:25:44 -0600

    changed connection to work with heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306, 
-    user: "node",
-    password: "12345",
-    database: "burgersDB"
-});
+var connection 
+
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306, 
+        user: "node",
+        password: "12345",
+        database: "burgersDB"
+    });
+}
+
 
 connection.connect((err)=> {
     if (err) {
